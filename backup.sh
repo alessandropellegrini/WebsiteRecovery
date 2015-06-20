0ac48963a4cb98e469f6cb52eee84a1f501cb153
Alessandro Pellegrini <alessandro@pellegrini.tk>
2015-06-20 23:11:45 +0200

    Multiple mementos mapped to latest available

--- backup.sh
+++ backup.sh
@@ -10,17 +10,44 @@ base_url="http://web.archive.org"
 
 
 get_initial_pool () {
+
+	echo -e "**** Querying the Wayback Machine for an initial list of archived files\n"
+
 	wget ${base_url}/web/*/http://www.${domain}/* -O raw-list
-	cat raw-list | grep href | grep -v link | grep -v Wayback | sed 's/">.*<\/a>//g' | sed 's/<a href="//g' | sed 's/^\t*//g' | sed 's/^ *//g' > list
+	cat raw-list | grep href | grep -v link | grep -v Wayback | sed 's/">.*<\/a>//g' | sed 's/<a href="//g' | sed 's/^\t*//g' | sed 's/^ *//g' > $1
+	unlink raw-list
 }
 
 
 differentiate() {
+
+	echo -e "**** Separating pages with multiple mementos from those with only one\n"
+
 	grep -v \* $1 > definitive
 	grep \* $1 > to-find-out
 }
 
+
+add_latest_to_list() {
+# $1 is the to-find-out list, $2 is the definitive list
+
+	echo -e "**** Getting the latest version of files with multiple mementos\n"
+
+	while IFS='' read -r line || [[ -n $line ]]; do
+		wget ${base_url}${line} -O mementos
+		latest=$(grep -A 3 between mementos | tail -n 1 | sed -n 's/.*href="\([^"]*\)".*/\1/p')
+		echo -e "Multiple mementos for $line \n resolved to $latest"
+		echo $latest >> $2
+		unlink mementos
+		sleep 1
+	done < $1
+}
+
+
 download_list() {
+
+	echo -e "**** Downloading the list of files\n"
+
 	while IFS='' read -r line || [[ -n $line ]]; do
 		destination=$(echo $line | sed 's/\/web\/[0-9a-z_]*\///g')
 		destination=$(echo $destination | sed "s/http:\/\/www\.${domain}\///g")
@@ -52,10 +79,10 @@ download_list() {
 
 
 
-get_initial_pool
+get_initial_pool list
 
 differentiate list
 
-
+add_latest_to_list to-find-out definitive
 
 #download_list definitive
