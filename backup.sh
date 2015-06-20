30249a64a07e4e2bd7757aead9c2c2a7d33a948c
Alessandro Pellegrini <alessandro@pellegrini.tk>
2015-06-20 23:40:39 +0200

    Function to extract new links from files

--- backup.sh
+++ backup.sh
@@ -44,6 +44,62 @@ add_latest_to_list() {
 }
 
 
+find_new_links() {
+# $1 is the just dowloaded file
+
+	echo -e "**** Scanning for additional unreferenced links\n"
+
+	name=$(basename $1)	
+	ext=${name##*.}
+	echo "extension in $1 is $ext"
+
+	# We just look into html files here
+	if [ "$ext" = "htm" ] || [ "$ext" = "html" ] || [ "$ext" = "php" ] || [ "$ext" = "php5" ]; then
+		grep href $1 > tmp-new-link
+
+		while IFS='' read -r line || [[ -n $line ]]; do
+			found_link=$(echo $line | sed -n 's/.*href="\([^"]*\)".*/\1/p')
+
+			if [ -z $found_link ]; then
+				continue;
+			fi
+			
+			# Discard the link if already present
+			if grep -Fxq "$found_link" definitive; then
+				echo "**** $found_link discarded, as it is already present in the list"
+			else
+				echo "**** Adding $found_link to the list of files to download"
+				echo $found_link >> additional
+			fi
+		done < tmp-new-link
+	fi
+
+	# CSS is another story...
+	if [ "$ext" = "css" ]; then
+		grep url $1 > tmp-new-link
+
+		while IFS='' read -r line || [[ -n $line ]]; do
+			found_link=$(echo $line | sed -n 's/.*url(\([^"]*\)).*/\1/p')
+
+			if [ -z $found_link ]; then
+				continue;
+			fi
+			
+			# Discard the link if already present
+			if grep -Fxq "$found_link" definitive; then
+				echo "**** $found_link discarded, as it is already present in the list"
+			else
+				echo "**** Adding $found_link to the list of files to download"
+				echo $found_link >> additional
+			fi
+		done < tmp-new-link
+	fi
+
+	exit
+	
+}
+
+
 download_list() {
 
 	echo -e "**** Downloading the list of files\n"
@@ -71,18 +127,22 @@ download_list() {
 		mkdir -p $path
 		wget ${base_url}$line -O $destination
 
+		find_new_links $destination
+
 		sleep 1
 	done < $1
 }
 
 
 
+echo "" > additional
 
 
-get_initial_pool list
 
-differentiate list
+#get_initial_pool list
 
-add_latest_to_list to-find-out definitive
+#differentiate list
 
-#download_list definitive
+#add_latest_to_list to-find-out definitive
+
+download_list definitive
