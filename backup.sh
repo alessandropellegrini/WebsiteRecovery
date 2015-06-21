a09ed93c1a57ce404115108e035d6baf98f406d3
Alessandro Pellegrini <alessandro@pellegrini.tk>
2015-06-21 11:49:54 +0200

    Find WBM links, iterate over new links

--- backup.sh
+++ backup.sh
@@ -13,7 +13,7 @@ get_initial_pool () {
 
 	echo -e "**** Querying the Wayback Machine for an initial list of archived files\n"
 
-	wget ${base_url}/web/*/http://www.${domain}/* -O raw-list
+	wget -q ${base_url}/web/*/http://www.${2}/* -O raw-list
 	cat raw-list | grep href | grep -v link | grep -v Wayback | sed 's/">.*<\/a>//g' | sed 's/<a href="//g' | sed 's/^\t*//g' | sed 's/^ *//g' > $1
 	unlink raw-list
 }
@@ -34,13 +34,29 @@ add_latest_to_list() {
 	echo -e "**** Getting the latest version of files with multiple mementos\n"
 
 	while IFS='' read -r line || [[ -n $line ]]; do
-		wget ${base_url}${line} -O mementos
+		wget -q ${base_url}${line} -O mementos
 		latest=$(grep -A 3 between mementos | tail -n 1 | sed -n 's/.*href="\([^"]*\)".*/\1/p')
 		echo -e "Multiple mementos for $line \n resolved to $latest"
 		echo $latest >> $2
 		unlink mementos
 		sleep 1
 	done < $1
+}
+
+
+# Check if a link is internal to Wayback Machine or not. In the latter case, try to extract an internal link from the service
+get_remote_link() {
+
+	remote_link=""
+
+	if grep -Gq "\/web\/[0-9a-z_]*\/" <<< "$1"; then
+		remote_link=$1
+	else
+		# Let's query here!
+		remote_link=$(wget -qO- ${base_url}/web/*/http://www.${domain}$1/* | grep -A 3 between | tail -n 1 | sed -n 's/.*href="\([^"]*\)".*/\1/p')
+	fi
+
+	echo $remote_link
 }
 
 
@@ -51,7 +67,6 @@ find_new_links() {
 
 	name=$(basename $1)	
 	ext=${name##*.}
-	echo "extension in $1 is $ext"
 
 	# We just look into html files here
 	if [ "$ext" = "htm" ] || [ "$ext" = "html" ] || [ "$ext" = "php" ] || [ "$ext" = "php5" ]; then
@@ -68,8 +83,14 @@ find_new_links() {
 			if grep -Fxq "$found_link" definitive; then
 				echo "**** $found_link discarded, as it is already present in the list"
 			else
-				echo "**** Adding $found_link to the list of files to download"
-				echo $found_link >> additional
+				echo -n "**** Checking if $found_link has a remote backup... "
+				found_link=$(get_remote_link $found_link)
+				if [ -z $found_link ]; then
+					echo "no. Cannot add it to the pool of files to download."
+				else
+					echo "yes. Adding it.\n"
+					echo $found_link >> additional
+				fi
 			fi
 		done < tmp-new-link
 	fi
@@ -89,14 +110,17 @@ find_new_links() {
 			if grep -Fxq "$found_link" definitive; then
 				echo "**** $found_link discarded, as it is already present in the list"
 			else
-				echo "**** Adding $found_link to the list of files to download"
-				echo $found_link >> additional
+				echo -n "**** Checking if $found_link has a remote backup... "
+				found_link=$(get_remote_link $found_link)
+				if [ -z $found_link ]; then
+					echo "no. Cannot add it to the pool of files to download."
+				else
+					echo "yes. Adding it.\n"
+					echo $found_link >> additional
+				fi
 			fi
 		done < tmp-new-link
 	fi
-
-	exit
-	
 }
 
 
@@ -125,24 +149,40 @@ download_list() {
 
 		echo -e "download $line \n to $destination \n in $path \n basename $name \n"
 		mkdir -p $path
-		wget ${base_url}$line -O $destination
+		wget -q ${base_url}$line -O $destination
 
 		find_new_links $destination
 
 		sleep 1
 	done < $1
+
+	cat $1 >> backup.log
 }
 
 
+check_complete() {
 
-echo "" > additional
+	echo -e "**** Checking if other files should be downloaded...\n"
+
+	unlink definitive 2> /dev/null
+	mv additional definitive 2> /dev/null
+
+	if [ -s "definitive" ]; then
+		return 1
+	fi
+
+	return 0
+}
 
 
+truncate -s 0 backup.log
+truncate -s 0 additional
 
-#get_initial_pool list
 
-#differentiate list
-
-#add_latest_to_list to-find-out definitive
-
-download_list definitive
+while : ; do
+	get_initial_pool list $domain
+	differentiate list
+	add_latest_to_list to-find-out definitive
+	download_list definitive
+	if check_complete ; then break; fi
+done
